c1b980465247b75c71cfdf8a26d36a4567b0f27f
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2021-11-08 16:18:10 +0100

    Added localStorage for Dark Mode, improved user interface

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
+	chk.checked?document.body.classList.add("dark"):document.body.classList.remove("dark");
+  localStorage.setItem('darkModeStatus', chk.checked)
 });
+
+
+  window.addEventListener('load', (event) => {
+  if(localStorage.getItem('darkModeStatus')=="true") {
+    document.body.classList.add("dark");
+    document.getElementById('chk').checked = true;
+  } 
+}
+  )
